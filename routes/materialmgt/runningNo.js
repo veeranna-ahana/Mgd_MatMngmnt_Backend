296c993fb26a758f8d452c67a8b7857f9c11ef93
ADITYA KUMAR <CodifyWithAditya@Gmail.com>
2024-04-18 17:14:20 +0530

    running in return...adi 18apr24

--- routes/materialmgt/runningNo.js
+++ routes/materialmgt/runningNo.js
@@ -187,4 +187,167 @@ runningNoRouter.post("/insertRunNoRow", async (req, res, next) => {
   }
 });
 
+runningNoRouter.post("/insertAndGetRunningNo", async (req, res, next) => {
+  // console.log("reqqqq", req.body);
+
+  let Running_No = 0;
+
+  const todayDate = new Date();
+
+  let finYear = `${
+    (todayDate.getMonth() + 1 < 4
+      ? todayDate.getFullYear() - 1
+      : todayDate.getFullYear()
+    )
+      .toString()
+      .slice(-2) +
+    "/" +
+    (todayDate.getMonth() + 1 < 4
+      ? todayDate.getFullYear()
+      : todayDate.getFullYear() + 1
+    )
+      .toString()
+      .slice(-2)
+  }`;
+
+  let Period = `${todayDate.getFullYear()}`;
+
+  try {
+    setupQueryMod(
+      `SELECT 
+            *
+          FROM
+            magod_setup.magod_runningno
+          WHERE
+            UnitName = '${req.body.unitName}' AND SrlType = '${req.body.srlType}'
+            AND ResetPeriod = '${req.body.ResetPeriod}'
+            AND Period = '${req.body.Period}'`,
+      (err, selectRN1) => {
+        if (err) {
+          logger.error(err);
+        } else {
+          // console.log("selectRN1", selectRN1);
+
+          if (selectRN1.length === 0) {
+            try {
+              setupQueryMod(
+                `SELECT 
+                      *
+                    FROM
+                        magod_setup.year_prefix_suffix
+                    WHERE
+                        UnitName = '${req.body.unitName}' AND SrlType = '${req.body.srlType}'`,
+                (err, selectYearPrefixSuffix) => {
+                  if (err) {
+                    logger.error(err);
+                  } else {
+                    // console.log(
+                    //   "selectYearPrefixSuffix",
+                    //   selectYearPrefixSuffix
+                    // );
+                    // let EffectiveFrom_date = `${
+                    //   todayDate.getFullYear() + "-04-01"
+                    // }`;
+                    // let Reset_date = `${
+                    //   todayDate.getFullYear() + 1 + "-03-31"
+                    // }`;
+                    setupQueryMod(
+                      `INSERT INTO magod_setup.magod_runningno
+                            (UnitName, SrlType, ResetPeriod, ResetValue, EffectiveFrom_date, Reset_date, Running_No, Prefix, Suffix, Length, Period, Running_EffectiveDate)
+                          VALUES
+                            ('${selectYearPrefixSuffix[0].UnitName || ""}', '${
+                        selectYearPrefixSuffix[0].SrlType || ""
+                      }', '${req.body.ResetPeriod || ""}', '${
+                        req.body.ResetValue || 0
+                      }', '${req.body.EffectiveFrom_date}', '${
+                        req.body.Reset_date
+                      }', '${Running_No}', '${
+                        selectYearPrefixSuffix[0].Prefix || ""
+                      }', '${selectYearPrefixSuffix[0].Suffix || ""}', '${
+                        req.body.Length || 5
+                      }', '${req.body.Period || ""}', now())`,
+                      (err, insertRunningNo) => {
+                        if (err) {
+                          logger.error(err);
+                        } else {
+                          // console.log("insertRunningNo", insertRunningNo);
+
+                          setupQueryMod(
+                            `SELECT * FROM magod_setup.magod_runningno WHERE Id = ${insertRunningNo.insertId}`,
+                            (err, selectRunningNo) => {
+                              if (err) {
+                                logger.error(err);
+                              } else {
+                                res.send({
+                                  runningNoData: selectRunningNo[0],
+                                  message: "running no inserted",
+                                });
+                                // console.log("selectRunningNo", selectRunningNo);
+                              }
+                            }
+                          );
+                        }
+                      }
+                    );
+                  }
+                }
+              );
+            } catch (error) {
+              next(error);
+            }
+          } else {
+            res.send({
+              runningNoData: selectRN1[0],
+              message: "fechted running no",
+            });
+          }
+        }
+      }
+    );
+  } catch (error) {
+    next(error);
+  }
+});
+
+runningNoRouter.post("/getAndUpdateRunningNo", async (req, res, next) => {
+  try {
+    setupQueryMod(
+      `SELECT 
+          *
+        FROM
+          magod_setup.year_prefix_suffix
+        WHERE
+          UnitName = '${req.body.runningNoData.UnitName}' AND SrlType = '${req.body.runningNoData.SrlType}'`,
+      (err, yearPrefixSuffixData) => {
+        if (err) {
+          logger.error(err);
+        } else {
+          setupQueryMod(
+            `UPDATE magod_setup.magod_runningno SET Running_No = '${parseInt(
+              req.body.newRunningNo
+            )}', Prefix = '${
+              yearPrefixSuffixData[0].Prefix || ""
+            }', Suffix = '${
+              yearPrefixSuffixData[0].Suffix || ""
+            }' WHERE (Id = '${req.body.runningNoData.Id}')`,
+            (err, updateRunningNo) => {
+              if (err) {
+                logger.error(err);
+              } else {
+                // console.log("updated running no");
+                res.send({
+                  flag: true,
+                  message: "running updated",
+                });
+              }
+            }
+          );
+        }
+      }
+    );
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = runningNoRouter;
