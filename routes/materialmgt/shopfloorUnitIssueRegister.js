bd6bdcd4a7634c8c93f5a9d35e19fa001eefaf96
Kanaka C S <kanaka.cs@ahanait.com>
2024-10-01 09:32:19 +0530

    Customer Bomlist ID issue resolved 01/10/24

--- routes/materialmgt/shopfloorUnitIssueRegister.js
+++ routes/materialmgt/shopfloorUnitIssueRegister.js
@@ -15,12 +15,14 @@ shopfloorUnitIssueRegisterRouter.get(
 
       //   let query = `SELECT * FROM magodmis.mtrlstocklist m WHERE m.cust_Code=${CustCode} AND
       // m.Mtrl_Code='${MtrlCode}' And  m.Locked=0 AND m.Scrap=0 `;
+      console.log("req.query", req.query);
 
-      let query = `SELECT * FROM magodmis.mtrlstocklist m WHERE m.cust_Code=${CustCode} AND 
-   m.Mtrl_Code='${MtrlCode}' And  m.Locked=0 AND m.Scrap=0 ORDER BY
-    SUBSTRING(MtrlStockID, 1, 8) ASC,   
-    CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(MtrlStockID, '/', -1), '/', 1) AS SIGNED) ASC, 
-    SUBSTRING(MtrlStockID, 9, 2) ASC`;
+      //     let query = `SELECT * FROM magodmis.mtrlstocklist m WHERE m.cust_Code=${CustCode} AND
+      //  m.Mtrl_Code='${MtrlCode}' And  m.Locked=0 AND m.Scrap=0 ORDER BY
+      //   SUBSTRING(MtrlStockID, 1, 8) ASC,
+      //   CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(MtrlStockID, '/', -1), '/', 1) AS SIGNED) ASC,
+      //   SUBSTRING(MtrlStockID, 9, 2) ASC`;
+      let query = `SELECT * FROM magodmis.mtrlstocklist m WHERE m.cust_Code = ${CustCode} AND m.Mtrl_Code = '${MtrlCode}' AND m.Locked = 0 AND m.Scrap = 0 ORDER BY SUBSTRING(MtrlStockID, 1, 8) ASC, SUBSTRING(MtrlStockID, 9, 2) ASC, CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(MtrlStockID, '/', -1), '/', 1) AS SIGNED) ASC;`;
 
       if (shape === "Sheet") {
         query += `AND ((DynamicPara1>=${para1} AND DynamicPara2>=${para2}) OR (DynamicPara2>=${para1} AND DynamicPara1>=${para2}))`;
@@ -31,10 +33,11 @@ shopfloorUnitIssueRegisterRouter.get(
       } else if (shape === "Tube Round") {
         query += `AND (DynamicPara1>=${para1} )`;
       }
-      console.log("query = ", query);
+      // console.log("query = ", query);
 
       await misQueryMod(query, (err, data) => {
         if (err) logger.error(err);
+        console.log("data1", data);
         res.send(data);
       });
     } catch (error) {
